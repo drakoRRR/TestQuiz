12c966a755c0a37c9d6a6652d909c7551cf4a9c0
Vlad <vmusaelyae@gmail.com>
2024-03-10 15:08:35 +0200

    added custom tag for templates, created view and template for testing

--- backend/testquizapp/main_app/urls.py
+++ backend/testquizapp/main_app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('', login_required(views.TestsPageView.as_view()), name='tests-page'),
     path('create-test/', login_required(views.CreateTestView.as_view()), name='create-test'),
     path('create-question/', views.CreateQuestionsView.as_view(), name='create-question'),
+
     path('api-create-question/', api_views.GetQuestions.as_view(), name='api-create-question'),
+
+    path('test-process/<int:test_id>/', views.TestProcessView.as_view(), name='test-process'),
 ]
--- backend/testquizapp/main_app/views.py
+++ backend/testquizapp/main_app/views.py
@@ -2,11 +2,21 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
 from .forms import TestQuizForm, QuestionForm, ChoiceForm
 from .models import TestQuiz, Choice, Question
+from django.db.models import Count
 
 
 class TestsPageView(ListView):
     model = TestQuiz
     template_name = 'main_app/tests_page.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TestsPageView, self).get_context_data()
+        context['tests_qty_questions'] = dict(
+            Question.objects.values('test_quiz_id').annotate(
+                questions_count=Count('id')
+            ).values_list('test_quiz_id', 'questions_count')
+        )
+        return context
 
 
 class CreateTestView(CreateView):
@@ -24,3 +34,26 @@ class CreateTestView(CreateView):
 
 class CreateQuestionsView(TemplateView):
     template_name = 'main_app/create_questions.html'
+
+
+class TestProcessView(ListView):
+    model = Question
+    template_name = 'main_app/test_process.html'
+
+    def get_queryset(self):
+        test_id = self.kwargs.get('test_id')
+        queryset = super(TestProcessView, self).get_queryset()
+        return queryset.filter(test_quiz_id=test_id)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TestProcessView, self).get_context_data(**kwargs)
+
+        test_id = self.kwargs.get('test_id')
+        questions_ids = Question.objects.filter(test_quiz_id=test_id).values_list('id', flat=True)
+        choices_dict = {question_id: Choice.objects.filter(question_id=question_id) for question_id in questions_ids}
+
+        context['choices_to_question'] = choices_dict
+        print(choices_dict)
+
+        return context
+
